67f47012fa8e812e7755a5343c00c7404801f39e
sairub <91753120+sairub@users.noreply.github.com>
2021-12-11 02:34:46 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
 # In[30]:
 
 
@@ -19,7 +25,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pylab
 
-from Prioritized_Replay import Replay_Agent_Memory
 from Agent import Agent
 from Network import DQN
 
@@ -29,22 +34,18 @@ env = gym.make('SpaceInvaders-v0')
 env.reset()
 
 height, width, channels = env.observation_space.shape
-print(env.observation_space.shape)
-print(env.action_space)
+print(height,width, channels)
 actions = env.action_space
-print(env.unwrapped.get_action_meanings())
-observation, reward, done, info = env.step(15)
+print(actions)
+observation, reward, done, info = env.step(3)
 #imgplot = plt.imshow(observation)
-
-
-# In[31]:
 
 
 print("Enter 'e' for Experience Replay and 'p' for Prioritized Experience Replay:")
 ans = input()
 
 
-# In[32]:
+# In[2]:
 
 
 if ans == 'e':
@@ -60,18 +61,13 @@ if ans == 'e':
     observation_new[2] = x3
 
     state = T.tensor([observation_new]).to(Q_eval.device)
-    actions = Q_eval.forward(state)
-    action = T.argmax(actions).item()
-
-    action_space = [0,1,2,3,4,5]
-    action = np.random.choice(action_space)
 
     env.reset()
 
     agent = Agent(ans, gamma=0.99,lr=0.001,epsilon=0.1,input_dims= observation_new.shape ,n_actions=env.action_space.n,batch_size=1,max_mem_size=1000,eps_end=0.01) 
 
     scores = []
-    n_games = 10
+    n_games = 100
     losses = []
     episodes = []
 
@@ -116,14 +112,13 @@ if ans == 'e':
     pylab.plot(episodes, scores, 'b')
 
 
-# In[33]:
+# In[3]:
 
 
 if ans == 'p':
     agent = Agent(ans, gamma=0.99,lr=0.001,epsilon=0.1,input_dims= env.observation_space.shape,n_actions=env.action_space.n,batch_size=1,max_mem_size=1000,eps_end=0.01) 
     
     Q_eval = DQN(ans,lr = 0.001, input_dims= env.observation_space.shape, fc1_dims = 300, fc2_dims = 100, n_actions = env.action_space.n)
-    Q_next = DQN(ans,lr = 0.001, input_dims= env.observation_space.shape, fc1_dims = 300, fc2_dims = 100, n_actions = env.action_space.n)
     
     x1 = observation[:,:,0]
     x2 = observation[:,:,1]
@@ -134,18 +129,13 @@ if ans == 'p':
     observation_new[2] = x3
 
     state = T.tensor([observation_new]).to(Q_eval.device)
-    actions = Q_eval.forward(state)
-
+    
     env.reset()
-    action = T.argmax(actions[1]).item()
-
-    action_space = np.arange(0,18,1)
-    action = np.random.choice(action_space)
     
     agent = Agent(ans, gamma=0.99,lr=0.001,epsilon=0.1,input_dims= observation_new.shape ,n_actions=env.action_space.n,batch_size=1,max_mem_size=1000,eps_end=0.01) 
 
     scores = []
-    n_games = 10
+    n_games = 100
     losses = []
     episodes = []
 
@@ -164,8 +154,8 @@ if ans == 'p':
 
         while not done:
 
-            #action = agent.choose_action(observation_new)
-            observation_, reward, done, info = env.step(15)
+            action = agent.choose_action(observation_new)
+            observation_, reward, done, info = env.step(action)
             x1 = observation_[:,:,0]
             x2 = observation_[:,:,1]
             x3 = observation_[:,:,2]
@@ -190,7 +180,6 @@ if ans == 'p':
 
 
 # In[ ]:
-
 
 
 
